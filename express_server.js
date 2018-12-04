3db4bf816ae71b25c48a2c2857892db22ff7f2f6
Ally <allison.avis@gmail.com>
2018-12-04 21:16:52 +0000

    generate random string function, added first post route

--- express_server.js
+++ express_server.js
@@ -2,12 +2,21 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+//helper functions
+//generates random string
+function generateRandomString(x) {
+  return [...Array(x)].map(i => (~~(Math.random() * 36)).toString(36)).join('');
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -25,12 +34,26 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//urls routes
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+//must go above :id route because it matches the pattern of /:id and get requests are rendered in order
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id };
   res.render("urls_show", templateVars);
 });
+
+
+
