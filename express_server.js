de04082cb5c236ef93f004406b47f39cc7029668
Ally <allison.avis@gmail.com>
2018-12-06 21:27:13 +0000

    registration page added, hardcoded some user objects for testing

--- express_server.js
+++ express_server.js
@@ -18,6 +18,19 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+}
+
 //helper functions
 //generates random string
 function generateRandomString(x) {
